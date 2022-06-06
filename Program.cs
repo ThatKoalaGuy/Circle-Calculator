617543b37f5d9d711f670392e4ffea127e2aac39
Domen Koder <domen.koder@gmail.com>
2022-06-06 19:57:03 +0200

    Working cicumference and radius

--- Program.cs
+++ Program.cs
@@ -10,25 +10,31 @@ namespace Circle_Calculator
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("---CIRCLE CALCULATOR---");
             Console.WriteLine("What do you wish to calculate?");
             Console.WriteLine("Cicumference (1)");
             Console.WriteLine("Surface area (2)");
 
             int choice = Convert.ToInt32(Console.ReadLine());
+
             if (choice == 1)
             {
-                Console.WriteLine("What is your radius?");
+                Console.WriteLine("What is your radius? (in cm)");
                 float r = Convert.ToInt64(Console.ReadLine());
 
-                //Console.WriteLine(r);
-
                 float cicumference = (float)(2 * 3.14 * r);
-                Console.WriteLine(cicumference);
-
-                Console.ReadLine();
+                Console.WriteLine($"{cicumference} cm2");
             }
 
+            if (choice == 2)
+            {
+                Console.WriteLine("What is your radius? (in cm)");
+                float r = Convert.ToInt64(Console.ReadLine());
+
+                float area = (float)(3.14 * r * r);
+                Console.WriteLine($"{area} cm2 ");
+            }
 
 
             Console.ReadLine();
